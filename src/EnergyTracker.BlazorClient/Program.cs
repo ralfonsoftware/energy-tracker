f68cb75fb0f306fcbdc58052b0e7ae31056caaaa
Ralf Ehlert <coding@ralfonsoftware.de>
2024-12-20 15:09:57 +0100

    Add MudBlazor

--- src/EnergyTracker.BlazorClient/Program.cs
+++ src/EnergyTracker.BlazorClient/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EnergyTracker.BlazorClient;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
