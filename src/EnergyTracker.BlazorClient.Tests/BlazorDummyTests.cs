7ff8be28f879b6ec5b7054abd6e15abde2545d22
Ralf Ehlert <coding@ralfonsoftware.de>
2024-12-20 14:52:05 +0100

    Create project skeletons

---
+++ src/EnergyTracker.BlazorClient.Tests/BlazorDummyTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace EnergyTracker.BlazorClient.Tests;
+
+public class BlazorDummyTests
+{
+    [Fact]
+    public void True_EvaluesToTrue()
+    {
+        var theBool = true;
+        theBool.Should().BeTrue();
+    }
+}
