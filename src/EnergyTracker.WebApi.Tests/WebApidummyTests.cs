e70c7a8f8ebded1c9506d8802b3e5c42a70aca9c
Ralf Ehlert <coding@ralfonsoftware.de>
2024-12-20 15:57:30 +0100

    Completes tech setup and closes #13

---
+++ src/EnergyTracker.WebApi.Tests/WebApidummyTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace EnergyTracker.WebApi.Tests;
+
+public class WebApidummyTests
+{
+    [Fact]
+    public void True_ShouldBeTrue()
+    {
+        var theBool = true;
+        theBool.Should().BeTrue();
+    }
+}
